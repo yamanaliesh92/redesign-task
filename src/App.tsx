4948909c5df99c07047da6d233119c3c07341330
Yaman Aliesh <Yaman9892xxx@gmail.com>
2025-06-08 20:35:10 +0300

    feat: add StepContent page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import ThemeToggle from "./components/themeToggle";
 import Header from "./layout/header";
+import StepContent from "./pages/stepContent";
 
 function App() {
   return (
     <div>
       <Header />
+      <StepContent />
       <ThemeToggle />
     </div>
   );
