45e1be7750069aa71d7da916ec5a45651460594b
Yaman Aliesh <alyamanAliesh@gmail.com>
2025-06-08 19:55:53 +0300

    feat: add ThemeContext   component

--- src/provider/provider.tsx
+++ src/provider/provider.tsx
@@ -2,5 +2,9 @@ import React, { ReactNode } from "react";
 import { StepProvider } from "../context/StepContext";
 
 export default function AppProvider({ children }: { children: ReactNode }) {
-  return <StepProvider>{children}</StepProvider>;
+  return (
+    <ThemeProvider>
+      <StepProvider>{children}</StepProvider>
+    </ThemeProvider>
+  );
 }
